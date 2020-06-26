569347a3aa73fe680a8e28df2f54efc86a6ccdd0
Marvin S <schroeder@mediameets.de>
2020-06-26 15:50:22 +0200

    Rename setup action to setup-deps, Fix package lock filename

--- sw6/dev/actions/setup-deps.sh
+++ sw6/dev/actions/setup-deps.sh
@@ -4,7 +4,7 @@
 ACTION: dev:composer-install
 
 if [ -f __SW6_ADMINISTRATION_ASSET_FOLDER__/package.json ]; then
-   if [ -f __SW6_ADMINISTRATION_ASSET_FOLDER__/package.lock ]; then
+   if [ -f __SW6_ADMINISTRATION_ASSET_FOLDER__/package-lock.json ]; then
    npm clean-install --prefix __SW6_ADMINISTRATION_ASSET_FOLDER__;
    else
    npm install --prefix __SW6_ADMINISTRATION_ASSET_FOLDER__;
@@ -12,7 +12,7 @@ if [ -f __SW6_ADMINISTRATION_ASSET_FOLDER__/package.json ]; then
    fi
 
 if [ -f __SW6_STOREFRONT_ASSET_FOLDER__/package.json ]; then
-   if [ -f __SW6_STOREFRONT_ASSET_FOLDER__/package.lock ]; then
+   if [ -f __SW6_STOREFRONT_ASSET_FOLDER__/package-lock.json ]; then
    npm clean-install --prefix __SW6_STOREFRONT_ASSET_FOLDER__;
    else
    npm install --prefix __SW6_STOREFRONT_ASSET_FOLDER__;
