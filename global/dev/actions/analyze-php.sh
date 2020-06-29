b44cfc9ac361c672e21cd0ba6a864c6454f5c450
Marvin S <schroeder@mediameets.de>
2020-06-29 12:16:05 +0200

    Replace file check with file executable check; Remove default psalm config

--- global/dev/actions/analyze-php.sh
+++ global/dev/actions/analyze-php.sh
@@ -2,10 +2,16 @@
 #DESCRIPTION: Analyzes the PHP code (with phpstan / psalm / phpinsights).
 
 # If phpstan/phpstan is installed, run it
-if [ -f vendor/bin/phpstan ]; then php vendor/bin/phpstan analyze; fi
+if [ -x vendor/bin/phpstan ]; then
+   php vendor/bin/phpstan analyze;
+   fi
 
 # If vimeo/psalm is installed, run it
-if [ -f vendor/bin/psalm ]; then php vendor/bin/psalm --config=psalm.xml --threads=$(nproc) --diff --diff-methods --show-info=false; fi
+if [ -x vendor/bin/psalm ]; then
+   php vendor/bin/psalm --threads=$(nproc) --diff --diff-methods --show-info=false;
+   fi
 
 # If nunomaduro/phpinsights installed, run it
-if [ -f vendor/bin/phpinsights ]; then php vendor/bin/phpinsights; fi
+if [ -x vendor/bin/phpinsights ]; then
+   php vendor/bin/phpinsights;
+   fi
--- global/dev/actions/fix-php.sh
+++ global/dev/actions/fix-php.sh
@@ -2,4 +2,6 @@
 #DESCRIPTION: Applys php-cs-fixer for PHP code.
 
 # If friendsofphp/php-cs-fixer is installed, run it
-if [ -f vendor/bin/php-cs-fixer ]; then php vendor/bin/php-cs-fixer fix; fi
+if [ -x vendor/bin/php-cs-fixer ]; then
+   php vendor/bin/php-cs-fixer fix;
+   fi
