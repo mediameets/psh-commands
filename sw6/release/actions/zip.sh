922847d0b72dc4c9a0bfebbe24a79b721094be8e
Marvin S <schroeder@mediameets.de>
2020-06-29 13:38:50 +0200

    Exclude tests folder and phpunit/xdebug config files from ZIP archive

--- sw6/release/actions/zip.sh
+++ sw6/release/actions/zip.sh
@@ -5,6 +5,7 @@ cd ../ && zip -FSr __PLUGIN_NAME__-__SW6_PLUGIN_VERSION__.zip __PLUGIN_NAME__ -x
    '__PLUGIN_NAME__/*.git/*'
    '__PLUGIN_NAME__/vendor/*'
    '__PLUGIN_NAME__/psh/*'
+   '__PLUGIN_NAME__/tests/*'
    '__PLUGIN_NAME__/composer.lock'
    '__PLUGIN_NAME__/__SW6_ADMINISTRATION_ASSET_FOLDER__/node_modules/*'
    '__PLUGIN_NAME__/__SW6_ADMINISTRATION_ASSET_FOLDER__/.stylelintignore'
@@ -21,6 +22,8 @@ cd ../ && zip -FSr __PLUGIN_NAME__-__SW6_PLUGIN_VERSION__.zip __PLUGIN_NAME__ -x
    '__PLUGIN_NAME__/phpinsights.php'
    '__PLUGIN_NAME__/phpstan.neon.dist'
    '__PLUGIN_NAME__/psalm.xml'
+   '__PLUGIN_NAME__/phpunit.xml.dist'
+   '__PLUGIN_NAME__/xdebug-filter.php'
 
 echo "HINT: Please check the content of the created ZIP archive! Override this sw6:release:zip command
    if you need to exclude additional files from the created ZIP archive!"
