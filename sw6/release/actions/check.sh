d12f05cfed3283ea9e1a575d4a57bd7887991fb7
Marvin S <schroeder@mediameets.de>
2020-06-30 11:08:40 +0200

    Update condition for checking releasability

--- sw6/release/actions/check.sh
+++ sw6/release/actions/check.sh
@@ -7,8 +7,8 @@ if [ -f __SW6_ADMINISTRATION_ASSET_FOLDER__/src/main.js ] && [ ! -d src/Resource
    exit 1;
    fi
 
-if [ -d __SW6_STOREFRONT_ASSET_FOLDER__/src ] && [ ! -d src/Resources/storefront/dist ];
-   then echo "ERROR: There need to be a src/Resources/storefront/dist folder
+if [ -f __SW6_STOREFRONT_ASSET_FOLDER__/src/main.js ] && [ ! -d __SW6_STOREFRONT_ASSET_FOLDER__/dist/storefront/js ];
+   then echo "ERROR: There need to be a src/Resources/storefront/dist/storefront/js folder
    with the compiled JavaScript assets for the storefront!";
    exit 1;
    fi
