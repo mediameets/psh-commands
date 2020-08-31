9c230c2a916046d80f76f64c9d05d8dde7936563
Marvin S <schroeder@mediameets.de>
2020-08-31 12:30:38 +0200

    Fix clean-up action by grouping or condition and removing file check

--- global/release/actions/clean-up.sh
+++ global/release/actions/clean-up.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 #DESCRIPTION: Deletes all unwanted files like .DS_Store, Thumbs.db..
 
-find ./ -name ".DS_Store"
+find ./ \(
+   -name ".DS_Store"
    -o -name "Thumbs.db"
    -o -name "__MACOSX"
-   -type f -delete
+   \)
+   -delete
